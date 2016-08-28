5ece3dc7812496f398c75e5b81c047a2a7171847
Burak Donbay <burak.donbay@gmail.com>
2016-08-28 14:06:54 +0300

    date.now added

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var port = process.env.port || 8080;
 http.createServer(function (req, res) {
     console.log('Demo HttpServer.');
     res.writeHead(200, { 'Content-Type': 'text/plain' });
+    console.log(Date.now());
     res.end('Hello beautiful!\n');
     
 }).listen(port);
